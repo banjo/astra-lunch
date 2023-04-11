337fcea84157d137185fbd7020890314475c6674
Anton Ödman <anton.odman@gmail.com>
2023-04-11 17:02:26 +0200

    chore: update time for worker

--- functions/src/func/schedule.ts
+++ functions/src/func/schedule.ts
@@ -11,7 +11,7 @@ export const fetchWorker = functions
 
 export const slackWorker = functions
     .region("europe-west1")
-    .pubsub.schedule("every weekday 10:00")
+    .pubsub.schedule("0 9 * * 1-5") // 9 every monday-friday
     .timeZone("Europe/Stockholm")
     .onRun(async () => {
         await sendToSlack();
