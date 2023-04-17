44e56ac88640838963dd26718c445ad18e9440f7
Anton Ödman <anton.odman@gmail.com>
2023-04-17 11:19:40 +0200

    chore: start fetching from 9

--- functions/src/func/schedule.ts
+++ functions/src/func/schedule.ts
@@ -3,7 +3,7 @@ import { fetchLunch, sendToSlack } from "../controller";
 
 export const fetchWorker = functions
     .region("europe-west1")
-    .pubsub.schedule("0 8-12 * * 1") // 8-12 every monday, once per hour
+    .pubsub.schedule("0 9-12 * * 1") // 8-12 every monday, once per hour
     .timeZone("Europe/Stockholm")
     .onRun(async () => {
         await fetchLunch();
