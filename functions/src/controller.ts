7b40dc4b07428962758fb42290d075cc7575360a
Anton Ödman <anton.odman@gmail.com>
2023-05-11 22:10:17 +0200

    chore: small refactor with success

--- functions/src/controller.ts
+++ functions/src/controller.ts
@@ -6,13 +6,20 @@ import { DateUtil } from "./utils/DateUtil";
 
 const fetchLunch = async () => {
     const success = await FetchService.fetchLunches();
+
+    Logger.log(`Success to fetch lunches: ${success}`);
+
+    if (!success) {
+        Logger.log("Failed to fetch lunches");
+        return;
+    }
+
     const weekNumber = DateUtil.getWeekNumber();
     const sent = await DatabaseService.hasSentLunchForWeek(weekNumber);
 
     Logger.log(`Has sent lunch for week ${weekNumber}: ${sent}`);
-    Logger.log(`Success to fetch lunches: ${success}`);
 
-    if (success && !sent) {
+    if (!sent) {
         await DatabaseService.setSentLunchForWeek(weekNumber);
         await SlackService.sendDailyLunch();
     }
